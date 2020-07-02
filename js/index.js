90997e12909d735a2810fd17af528a61e55aa921
KilFront <kaifawebb@gmail.com>
2020-07-02 14:23:48 +0800

    update

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// import * as mod1 from './mod1'
+
+// console.log(mod1.a, mod1.b, mod1.c);
+let b = 8
+
+import {a, b as name2} from './mod1';
+
+console.log(a, b, name2);
+
+
---
+++ js/mod1.js
@@ -0,0 +1,3 @@
+export let a=12;
+export let b=5;
+export let c=6;
