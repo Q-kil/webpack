33130d2014fa8c42beadde9363e40b896c6a6d18
KilFront <kaifawebb@gmail.com>
2020-07-03 14:15:35 +0800

    es6

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import $ from 'jquery';
+let a = 12;
+let b = 13;
 
-$(function() {
-  $('body').css('background', '#CCC');
-  alert('a');
-  console.log('env', process.env.NODE_ENV);
-  
-})
+let sum=(n1, n2) => n1 + n2;
+
+alert(sum2(a,b));
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: {
-    index: './src/index.js',
-    news: './src/news.js'
+    index: './src/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'dest'),
     // [name]: 会被替换，替换成entry里面的key值
     filename: '[name].js'
-  }
+  },
+  module: {
+    rules: [
+      {test: /\.js$/i, use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }}
+    ]
+  },
+  // 开发时，便于调试
+  devtool: 'source-map'
 }
