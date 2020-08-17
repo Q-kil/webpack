2b9416e59075e5075641578d60a4be731ba1feee
KilFront <kaifawebb@gmail.com>
2020-08-17 13:51:12 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   mode: 'production',
@@ -10,5 +11,12 @@ module.exports = {
     path: path.resolve(__dirname, 'dest'),
     // [name]: 会被替换，替换成entry里面的key值
     filename: '[name].js'
-  }
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'test_copy_plugin.js', to: path.resolve(__dirname, 'dest') }
+      ]
+    })
+  ]
 }
